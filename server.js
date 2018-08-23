4ede5689061bcd6b578f22029669455a23712c6f
ericwdq <wdq1103@163.com>
2018-08-23 15:18:37 +0800

    update title

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 "use strict";
 
 // Optional. You will see this name in eg. 'ps' or 'top' command
-process.title = "node-chat";
+process.title = "node-ws-chat";
 
 // Port where we'll run the websocket server
 const webSocketsServerPort = 1337;
